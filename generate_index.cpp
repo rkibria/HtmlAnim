a7743f9f4e5c0572e45a972c76f1f88c213d92ed
Raihan Kibria <rkibria@users.noreply.github.com>
2019-03-03 16:04:15 +0100

    Improve index page.

--- generate_index.cpp
+++ generate_index.cpp
@@ -1,29 +1,41 @@
 #include "htmlanim.hpp"
+#include <ctgmath>
 
 int main() {
-	HtmlAnim::HtmlAnim anim("HtmlAnim", 400, 300, 20);
+	HtmlAnim::HtmlAnim anim("HtmlAnim", 400, 300, 5);
 
 	anim.set_pre_text(R"(
-<h1>HtmlAnim - HTML animation library</h1>
-<p>
-Github project: <a href="https://github.com/rkibria/HtmlAnim">https://github.com/rkibria/HtmlAnim</a>
-</p>
+<h1>HtmlAnim</h1>
+<h2>A header-only C++ HTML animation library</h2>
 )");
 
 	anim.set_post_text(R"(
+<p>The animation above can be generated using this code:</p>
+<p><pre>
+#include "htmlanim.hpp"
+int main() {
+	HtmlAnim::HtmlAnim anim("HtmlAnim", 400, 300, 5);
+
+	anim.write_file("index.html");
+}
+</pre></p>
 <hr>
-<small>Generated with HtmlAnim</small>
+<p>
+<small>Github project: <a href="https://github.com/rkibria/HtmlAnim">https://github.com/rkibria/HtmlAnim</a></small>
+</p>
 )");
 
-	anim.text(30, 25, "Frame ${frame_counter + 1}/${num_frames}");
-	anim.rectangle(10, 5, 30, 40);
-	anim.rectangle(30, 25, 80, 50, true);
-	anim.text(30, 99, "This is text!");
-	anim.next_frame();
+	auto marker = [&anim](auto x, auto y) {
+		anim.rectangle(static_cast<int>(x), static_cast<int>(y), 10, 10);
+	};
 
-	anim.text(30, 25, "Frame ${frame_counter + 1}/${num_frames}");
-	anim.rectangle(50, 50, 90, 60);
-	anim.rectangle(10, 40, 30, 70, true);
+	for(int i = 0; i < 20; ++i) {
+		for(int n = 0; n < 30; ++n) {
+			const auto x = static_cast<double>(n) + static_cast<double>(i) / 5.0;
+			marker(x * 15, 150 + 100 * sin(x / 2.0));
+		}
+		anim.next_frame();
+	}
 
 	anim.write_file("index.html");
 }
