d046a002076a2e4fe1fd61aa7c5512f0034ef742
Raihan Kibria <rkibria@users.noreply.github.com>
2019-05-29 20:56:02 +0200

    Logo text

--- docs/logo.cpp
+++ docs/logo.cpp
@@ -6,26 +6,36 @@ int main() {
 	const auto height = 450;
 	HtmlAnim::HtmlAnim anim("Logo", width, height);
 
-	anim.background().save().fill_style("white").rect(0, 0, anim.get_width(), anim.get_height(), true);
+	anim.background()
+		.font("bold 160px sans-serif")
+		.save().fill_style("white").rect(0, 0, anim.get_width(), anim.get_height(), true);
 
-	std::vector<std::string> rainbow_cols = { "#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000" };
+	const std::vector<std::string> rainbow_cols = { "#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000" };
 
 	const auto total_time = 5;
+	const auto radius = 60;
+	const auto right_edge = width - radius - 10;
+	const auto lower_edge = height - radius - 10;
+
+	anim.frame().text(
+		10,
+		anim.frame().linear_range(-100.0, lower_edge - radius - 10, 1 * HtmlAnim::FPS),
+		"HtmlAnim"
+		);
+
 	const auto n_cols = rainbow_cols.size();
 	for (size_t i = 0; i < n_cols; ++i) {
 		const auto inv_i = n_cols - 1 - i;
 		const auto n_points = 9 - i;
-		const auto radius = 60;
-		const auto right_edge = width - radius - 10;
 		anim.frame()
 			.wait(total_time * HtmlAnim::FPS)
 			.save()
 			.fill_style(rainbow_cols[i])
 			.translate(anim.frame().linear_range(
 					right_edge,
-					right_edge - (inv_i) * radius * 1.75,
+					right_edge - (inv_i) * radius * 1.85,
 					1.5 * HtmlAnim::FPS),
-				height - radius - 10)
+				lower_edge)
 			.rotate(anim.frame().linear_range(0, 2 * HtmlAnim::PI * (i + 1), total_time * HtmlAnim::FPS))
 			.add_drawable(HtmlAnimShapes::regular_polygon(0, 0, radius, n_points, true));
 	}
