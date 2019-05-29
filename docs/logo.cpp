1ae20831eadd62028d9d24534f708a9dad440190
Raihan Kibria <rkibria@users.noreply.github.com>
2019-05-29 20:05:52 +0200

    Logo

--- docs/logo.cpp
+++ docs/logo.cpp
@@ -10,14 +10,23 @@ int main() {
 
 	std::vector<std::string> rainbow_cols = { "#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000" };
 
-	for (size_t i = 0; i < rainbow_cols.size(); ++i) {
+	const auto total_time = 5;
+	const auto n_cols = rainbow_cols.size();
+	for (size_t i = 0; i < n_cols; ++i) {
+		const auto inv_i = n_cols - 1 - i;
 		const auto n_points = 9 - i;
 		const auto radius = 60;
+		const auto right_edge = width - radius - 10;
 		anim.frame()
+			.wait(total_time * HtmlAnim::FPS)
 			.save()
 			.fill_style(rainbow_cols[i])
-			.translate(anim.frame().linear_range(700, 40 + i * radius, 5 * HtmlAnim::FPS), 150)
-			.rotate(anim.frame().linear_range(0, 2 * HtmlAnim::PI, 3 * HtmlAnim::FPS))
+			.translate(anim.frame().linear_range(
+					right_edge,
+					right_edge - (inv_i) * radius * 1.75,
+					1.5 * HtmlAnim::FPS),
+				height - radius - 10)
+			.rotate(anim.frame().linear_range(0, 2 * HtmlAnim::PI * (i + 1), total_time * HtmlAnim::FPS))
 			.add_drawable(HtmlAnimShapes::regular_polygon(0, 0, radius, n_points, true));
 	}
 
