c099c743d7b9e2cc33eeef0892e27183b64d293d
Raihan Kibria <rkibria@users.noreply.github.com>
2019-05-30 21:41:42 +0200

    Logo

--- docs/generate_index.cpp
+++ docs/generate_index.cpp
@@ -57,43 +57,43 @@ void make_index() {
 		.rect(0, 0, anim.get_width(), anim.get_height(), true);
 	anim.add_layer();
 
-	const std::vector<std::string> rainbow_cols = { "#9400D3", "#4B0082", "#0000FF", "#00FF00", "#FFFF00", "#FF7F00", "#FF0000" };
+	const std::vector<std::string> rainbow_clrs = { "#9400D3", "#4B0082", "#0000FF", "#00FF00",
+		"#FFFF00", "#FF7F00", "#FF0000" };
 
 	const auto total_time = 5;
-	const auto radius = 60;
-	const auto right_edge = width - radius - 10;
-	const auto lower_edge = height - radius - 10;
+	const auto lower_edge = height - 70;
 
-	anim.frame()
-		.font("bold 160px sans-serif")
-		.text(
-		10,
-		anim.frame().linear_range(-100.0, lower_edge - radius - 10, 1 * HtmlAnim::FPS),
-		"Html"
-		);
-
-	const auto n_cols = rainbow_cols.size();
-	for (size_t i = 0; i < n_cols; ++i) {
-		const auto inv_i = n_cols - 1 - i;
-		const auto n_points = 9 - i;
+	const auto rainbow_linewidth = 30;
+	const auto radius = 300;
+	for (size_t i = 0; i < rainbow_clrs.size(); ++i) {
 		anim.frame()
 			.wait(total_time * HtmlAnim::FPS)
 			.save()
-			.fill_style(rainbow_cols[i])
-			.translate(anim.frame().linear_range(
-					right_edge,
-					right_edge - (inv_i) * radius * 1.85,
-					1.5 * HtmlAnim::FPS),
-				lower_edge)
-			.rotate(anim.frame().linear_range(0, 2 * HtmlAnim::PI * (i + 1), total_time * HtmlAnim::FPS))
-			.add_drawable(HtmlAnimShapes::regular_polygon(0, 0, radius, n_points, true));
+			.stroke_style(rainbow_clrs[i])
+			.line_width(rainbow_linewidth + 1)
+			.arc(anim.get_width() / 2,
+				anim.frame().linear_range(anim.get_height() + radius + rainbow_linewidth, anim.get_height() + 50, 3 * HtmlAnim::FPS),
+				radius - i * rainbow_linewidth);
 	}
 
-	anim.frame().text(
-		370,
-		anim.frame().linear_range(height + 100, lower_edge - radius, 1 * HtmlAnim::FPS),
-		"Anim"
-	);
+	const auto& text_1_expression = anim.frame().linear_range(-100.0, lower_edge - 10, 1 * HtmlAnim::FPS);
+	anim.frame()
+		.font("bold 160px sans-serif")
+		.save()
+		.line_width(5)
+		.stroke_style("gray")
+		.fill_style("lightgray")
+		.text(10, text_1_expression, "Html")
+		.text(10, text_1_expression, "Html", false);
+
+	const auto& text_2_expression = anim.frame().linear_range(height + 100, lower_edge, 1 * HtmlAnim::FPS);
+	anim.frame()
+		.save()
+		.line_width(5)
+		.stroke_style("gray")
+		.fill_style("lightgray")
+		.text(370, text_2_expression, "Anim")
+		.text(370, text_2_expression, "Anim", false);
 
 	anim.write_file("index.html");
 }
