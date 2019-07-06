5052cb3354d5c23686fd93fc87cf6d4a3f7f33b1
Raihan Kibria <rkibria@users.noreply.github.com>
2019-07-06 12:04:46 +0200

    Refactor EA

---
+++ docs/ea_base.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <iomanip>
+#include <thread>
+
+namespace EA {
+
+using FitnessType = std::vector<double>;
+using GeneType = double;
+using GeneVector = std::vector<GeneType>;
+
+class SolutionBase {
+protected:
+	FitnessType fitness;
+	GeneVector gene_vec;
+
+	auto get_random_seed() {
+		return static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count());
+	}
+
+public:
+	explicit SolutionBase(size_t fitness_size, size_t s)
+		: fitness(fitness_size, 0), gene_vec(s, 0) {
+	}
+
+	virtual ~SolutionBase() {}
+
+	virtual SolutionBase& operator=(const SolutionBase& rhs) {
+		gene_vec = rhs.gene_vec;
+		return *this;
+	}
+
+	virtual const FitnessType& get_fitness() const { return fitness; }
+
+	virtual void randomize(GeneType lb, GeneType ub) {
+		std::default_random_engine generator(get_random_seed());
+		std::uniform_real_distribution<GeneType> distribution(lb, ub);
+		for (auto& g : gene_vec) {
+			g = distribution(generator);
+		}
+	}
+
+	virtual void mutate(GeneType mean, GeneType stddev) {
+		std::default_random_engine generator(get_random_seed());
+		std::normal_distribution<GeneType> distribution(mean, stddev);
+		for (auto& g : gene_vec) {
+			const auto r = distribution(generator);
+			g += r;
+		}
+	}
+
+	virtual void evaluate() = 0;
+};
+
+using SolutionVector = std::vector<std::unique_ptr<SolutionBase>>;
+
+template<class T>
+class Population {
+	SolutionVector sol_vec;
+
+	static const constexpr size_t n_threads = 8;
+
+	void sort_by_fitness() {
+		std::sort(sol_vec.begin(), sol_vec.end(),
+			[](const auto& sol1, const auto& sol2) {
+				return sol1->get_fitness() < sol2->get_fitness(); 
+			});
+	}
+
+	void evaluate(GeneType mutation_stddev) {
+		std::vector<std::thread> threads;
+		if (sol_vec.size() % n_threads != 0) {
+			throw std::exception("Population size must be multiple of n_threads");
+		}
+
+		auto evaluate_thread = [this, mutation_stddev](size_t start_i, size_t end_i) {
+			for (size_t i = start_i; i < end_i; ++i) {
+				auto& sol = sol_vec[i];
+				sol->mutate(0, mutation_stddev);
+				sol->evaluate();
+			}
+		};
+
+		const auto batch_size = sol_vec.size() / n_threads;
+		for (size_t i = 0; i < n_threads; ++i) {
+			threads.push_back(std::thread(evaluate_thread, i * batch_size, (i + 1) * batch_size));
+		}
+
+		for (auto& t : threads) {
+			if (t.joinable())
+				t.join();
+		}
+	}
+
+public:
+	Population(size_t s) {
+		sol_vec.resize(s);
+		for (size_t i = 0; i < s; ++i) {
+			sol_vec[i] = std::make_unique<T>();
+		}
+
+		evaluate(0);
+		sort_by_fitness();
+	}
+
+	const auto& get_best() const {
+		return sol_vec.front();
+	}
+
+	void evolve(GeneType stddev) {
+		for (size_t i = 0; i < sol_vec.size() / 2; ++i) {
+			*sol_vec[sol_vec.size() / 2 + i] = *sol_vec[i];
+		}
+
+		evaluate(stddev);
+		sort_by_fitness();
+	}
+
+};
+
+} // namespace EA
--- docs/ea_demo.cpp
+++ docs/ea_demo.cpp
@@ -1,57 +1,21 @@
 #include <htmlanim.hpp>
 #include <htmlanim_shapes.hpp>
 
-#include <chrono>
-#include <random>
-#include <algorithm>
-#include <iomanip>
-#include <thread>
+#include "ea_base.h"
 
-using FitnessType = std::vector<double>;
-using GeneType = double;
-using GeneVector = std::vector<GeneType>;
+using namespace EA;
 
-class Solution {
+class CirclesSolution : public SolutionBase {
 	static constexpr size_t fitness_size = 2;
-	FitnessType fitness;
-	GeneVector gene_vec;
-
-	auto get_random_seed() {
-		return static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count());
-	}
+	static constexpr size_t num_circles = 10;
 
 	GeneType radius(size_t i) const {
 		return static_cast<GeneType>(i/5 + 1) * 5.0;
 	}
 
 public:
-	Solution() : fitness(fitness_size, 0) {}
-
-	Solution(size_t s) : fitness(fitness_size, 0), gene_vec(s * 2, 0) {
-	}
-
-	Solution& operator=(const Solution& rhs) {
-		gene_vec = rhs.gene_vec;
-		return *this;
-	}
-
-	const auto& get_fitness() const { return fitness; }
-
-	void randomize(GeneType lb, GeneType ub) {
-		std::default_random_engine generator(get_random_seed());
-		std::uniform_real_distribution<GeneType> distribution(lb, ub);
-		for (auto& g : gene_vec) {
-			g = distribution(generator);
-		}
-	}
-
-	void mutate(GeneType mean, GeneType stddev) {
-		std::default_random_engine generator(get_random_seed());
-		std::normal_distribution<GeneType> distribution(mean, stddev);
-		for (auto& g : gene_vec) {
-			const auto r = distribution(generator);
-			g += r;
-		}
+	CirclesSolution() : SolutionBase(fitness_size, num_circles * 2) {
+		randomize(-300, 300);
 	}
 
 	void draw(HtmlAnim::HtmlAnim& anim) const {
@@ -62,7 +26,7 @@ public:
 		}
 	}
 
-	void evaluate() {
+	virtual void evaluate() override {
 		std::fill(fitness.begin(), fitness.end(), 0);
 		for (size_t i = 0; i < gene_vec.size() / 2; ++i) {
 			const auto x1 = gene_vec[2 * i];
@@ -93,88 +57,16 @@ public:
 	}
 };
 
-using SolutionVector = std::vector<std::unique_ptr<Solution>>;
-
-class Population {
-	SolutionVector sol_vec;
-
-	static const constexpr size_t n_threads = 8;
-
-	void sort_by_fitness() {
-		std::sort(sol_vec.begin(), sol_vec.end(),
-			[](const auto& sol1, const auto& sol2) {
-				return sol1->get_fitness() < sol2->get_fitness(); 
-			});
-	}
-
-	void evaluate(GeneType mutation_stddev) {
-		std::vector<std::thread> threads;
-		if (sol_vec.size() % n_threads != 0) {
-			throw std::exception("Population size must be multiple of n_threads");
-		}
-
-		auto evaluate_thread = [this, mutation_stddev](size_t start_i, size_t end_i) {
-			for (size_t i = start_i; i < end_i; ++i) {
-				auto& sol = sol_vec[i];
-				sol->mutate(0, mutation_stddev);
-				sol->evaluate();
-			}
-		};
-
-		const auto batch_size = sol_vec.size() / n_threads;
-		for (size_t i = 0; i < n_threads; ++i) {
-			threads.push_back(std::thread(evaluate_thread, i * batch_size, (i + 1) * batch_size));
-		}
-
-		for (auto& t : threads) {
-			if (t.joinable())
-				t.join();
-		}
-	}
-
-	void randomize() {
-		for (auto& sol : sol_vec) {
-			sol->randomize(-300, 300);
-		}
-	}
-
-public:
-	Population(size_t s, size_t n_genes) {
-		sol_vec.resize(s);
-		for (size_t i = 0; i < s; ++i) {
-			sol_vec[i] = std::make_unique<Solution>(n_genes);
-		}
-		randomize();
-		evaluate(0);
-		sort_by_fitness();
-	}
-
-	const auto& get_best() const {
-		return sol_vec.front();
-	}
-
-	void evolve(GeneType stddev) {
-		for (size_t i = 0; i < sol_vec.size() / 2; ++i) {
-			*sol_vec[sol_vec.size() / 2 + i] = *sol_vec[i];
-		}
-
-		evaluate(stddev);
-
-		sort_by_fitness();
-	}
-
-};
-
 int main() {
 	HtmlAnim::HtmlAnim anim("Evolutionary algorithm", 600, 600);
 	anim.frame().save().fill_style("white").rect(0, 0, anim.get_width(), anim.get_height(), true);
 	anim.frame().add_drawable(HtmlAnimShapes::subdivided_grid(0, 0, 50, 50, 12, 12, 5, 5));
 	anim.add_layer();
 
-	Population pop(10000, 25);
+	Population<CirclesSolution> pop(10000);
 
 	FitnessType best_fitness;
-	Solution best_solution;
+	CirclesSolution best_solution;
 
 	const auto start_time = std::chrono::high_resolution_clock::now();
 
@@ -188,7 +80,7 @@ int main() {
 		const auto& current_best = pop.get_best();
 		if (i == 0 || current_best->get_fitness() < best_fitness) {
 			best_fitness = current_best->get_fitness();
-			best_solution = *current_best;
+			best_solution = dynamic_cast<CirclesSolution&>(*current_best);
 		}
 		best_solution.draw(anim);
 
