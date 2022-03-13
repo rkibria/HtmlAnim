93d137b7cb6608f80db5d5d7cb41ca3ba3d88dd5
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2022-03-13 02:54:38 +0000

    Implement surface and drawImage

--- docs/offscreens.cpp
+++ docs/offscreens.cpp
@@ -3,6 +3,13 @@
 
 int main() {
 	HtmlAnim::HtmlAnim anim("Offscreens", 600, 600);
+    anim.set_num_surfaces(1);
+
+    anim.frame().surface(0).line(0,0, 600, 600).line(0,600, 600, 0);
+    anim.frame().save().stroke_style("red").line(0,0, 600, 600).line(0,600, 600, 0);
+    anim.frame().drawImage(0,
+        0, 0, 600, 600,
+        0, 0, 300, 300);
 
 	anim.write_file("offscreens.html");
 }
